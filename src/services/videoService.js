ba74866d2b721d4297dcfe872f5b1714a32b89e1
Gregory Perez <Gregory10183@gmail.com>
2023-08-19 19:14:05 -0400

    Updating api URL

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3000/Videos";
+const baseUrl = "https://aluraflixapi.gregorypf.site/Videos";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
