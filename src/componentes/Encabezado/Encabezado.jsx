ea07d6d345ea53b95704c35321f8d248659fe3de
Gregory Perez <Gregory10183@gmail.com>
2023-08-20 00:16:31 -0400

    ajustando forms para resoluciones mayores

--- src/componentes/Encabezado/Encabezado.jsx
+++ src/componentes/Encabezado/Encabezado.jsx
@@ -1,14 +1,23 @@
 import "./Encabezado.css";
 import Boton from "../Button/Boton";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Encabezado() {
+  const location = useLocation();
+
+  console.log(location.pathname);
+
   return (
     <section className="encabezado">
       <Link to={"/"}>
         <img src="/Logo.png" alt="" />
       </Link>
-      <Boton text={"Nuevo Video"} type={"nuevoVideoHeader"} />
+
+      {location.pathname === "/" && (
+        <Link to={"/nuevoVid"}>
+          <Boton text={"Nuevo Video"} type={"nuevoVideoHeader"} />
+        </Link>
+      )}
     </section>
   );
 }
